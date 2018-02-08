cf66ff6d91b0f326a83b3e59965fef15e2bd1b69
mengyuchen <mengyuchen@umail.ucsb.edu>
2018-02-07 23:33:52 -0800

    agentsv2

--- agents/agents.cpp
+++ agents/agents.cpp
@@ -10,13 +10,12 @@ using namespace std;
 double scaleFactor = 0.1;  
 double coneRadius = 2;
 double coneHeight = 6;
-double borderRadius = 120;
-double boundary_radius = 120; //a spherical boundary with center at (0,0,0)
+double sphereRadius = 3;
+double boundary_radius = 200; //a spherical boundary with center at (0,0,0)
 Vec3f boundary_origin(0,0,0); //boundary center as (0,0,0)
-int clan_population_max = 50;
+int clan_population_max = 50; //max boids in the scene
 Mesh cone;
-Vec3f target(0,0,0);
-
+Mesh sphere;
 
 // helper function: makes a random vector
 Vec3f r() { return Vec3f(rnd::uniformS(), rnd::uniformS(), rnd::uniformS()); }
@@ -48,6 +47,87 @@ struct Frame : Mesh {
         }
 };
 
+struct Target{
+    Vec3f position, velocity, acceleration;
+    float lifespan;
+    Color c;
+    float initialRadius;
+    float initialSpeed;
+    float mass;
+    float maximumAcceleration;
+    float lifeDecaySpeed;
+    Target(){
+        initialRadius = 60;
+        initialSpeed = 2.5;
+        position = r() * initialRadius;
+        mass = 1;
+        maximumAcceleration = 30;
+        lifeDecaySpeed = 0.0001;
+        velocity = Vec3f(0, 1, 0).cross(position).normalize(initialSpeed);
+    c = HSV(rnd::uniform(), 0.7, 1);
+    lifespan = rnd::uniform() * 1.0;
+    }
+    void applyForce(){
+      Vec3f f = acceleration / mass;
+      acceleration += f;
+      if (acceleration.mag() > maximumAcceleration){
+        acceleration.normalize(maximumAcceleration);
+      }
+      acceleration.zero();
+      //velocity += spring_force; //collision spring effect
+    }
+    void update(){
+      velocity += acceleration;
+      position += velocity;
+      lifespan -= lifeDecaySpeed;
+    }
+    void boundary_detect(){
+      Vec3f d = position - boundary_origin;
+      if (d > boundary_radius) {
+          velocity *= -1;
+      }
+    }
+    void draw(Graphics& g) {
+    g.pushMatrix();
+    g.translate(position);
+    g.color(c);
+    g.draw(sphere);
+    g.popMatrix();
+    }
+     bool isDead(){
+      if (lifespan < 0.0){
+          return true;
+      } else {
+          return false;
+      }
+  }
+};
+struct Target_group{
+    vector<Target> targets;
+    Target_group(){
+    }
+    void add_target(){
+        Target t;
+        targets.push_back(t);
+    }
+    void applyBehaviors(){
+        for (Target& t : targets) {
+            t.applyForce();
+            t.boundary_detect();
+            t.update();
+        }
+    }
+    void draw(Graphics& g){
+        for (int i = targets.size() - 1; i >= 0; i--){
+            Target& t = targets[i];
+            t.draw(g);
+            if (t.isDead()){
+                targets.erase(targets.begin() + i);
+            }
+        }
+    }
+};
+
 struct Boid {
     Vec3f velocity, acceleration;
     Pose pose;
@@ -56,7 +136,8 @@ struct Boid {
     float mass;
     float maxforce;
     float initialRadius; // spawn area
-    float senseRadius;
+    float neighbour_senseRadius;
+    float target_senseRadius;
     float desiredseparation;
     Color c;
     Quatd q;
@@ -68,8 +149,9 @@ struct Boid {
         maxspeed = 4;
         maxforce = 0.1;
         initialRadius = 50;
-        senseRadius = 60;
-        desiredseparation = 10;
+        neighbour_senseRadius = 30;
+        target_senseRadius = 90;
+        desiredseparation = 20;
 
         acceleration = Vec3f(0,0,0);
         velocity = Vec3f(0,0,0);
@@ -92,12 +174,12 @@ struct Boid {
         return steer;
     }
 
-    void arrive(Vec3f target){
+    Vec3f arrive(Vec3f target){
         Vec3f desired = target - pose.pos();
         float d = desired.mag();
         desired.normalize();
-        if (d < senseRadius){
-            float m = MapValue(d, 0, senseRadius, 0, maxspeed);
+        if (d < target_senseRadius){
+            float m = MapValue(d, 0, target_senseRadius, 0, maxspeed);
             desired *= m;
         } else {
             desired *= maxspeed;
@@ -106,7 +188,7 @@ struct Boid {
         if (steer.mag() > maxforce){
             steer.normalize(maxforce);
         }
-        applyForce(steer);
+        return steer;
     }
 
     void update(){
@@ -117,28 +199,38 @@ struct Boid {
         pose.pos() += velocity;
         acceleration *= 0; //zeros acceleration
     }
+
+    //not used, only for special single target
     Quatd facing_single_target(Vec3f t){
         Quatd q;
         Vec3f src = Vec3f(pose.quat().toVectorZ()).normalize();
         Vec3f dst = Vec3f(t - pose.pos()).normalize();
-        Vec3f desired = target - pose.pos();
+        Vec3f desired = t - pose.pos();
         float d = desired.mag();
         Quatd rot = Quatd::getRotationTo(src,dst);
-        
-        if (d < senseRadius){
-            q = rot * q;
-            return q;
-        } else {
-            return q;
-        }
+        return q;
     }
-    void follow_group_direction(vector<Boid> c){
+    void find_direction(vector<Boid> c, Target_group tg){
         Vec3f sum(0,0,0);
         int count = 0;
+        double min = 99999;
+        int min_id;
+
+        //find closest target
+        for (int i = 0; i < tg.targets.size(); i ++){
+            Vec3f t_difference = pose.pos() - tg.targets[i].position;
+            double td = t_difference.mag();
+            if (td < min){
+                min = td;
+                min_id = i;
+            }     //need to find the nearest target
+        }
+
+        //find center of clan position
         for (Boid other : c){
             Vec3f difference = pose.pos() - other.pose.pos();
             double d = difference.mag();
-            if ((d > 0) && (d < senseRadius)){
+            if ((d > 0) && (d < neighbour_senseRadius)){
                 //sum += other.velocity;
                 sum += other.pose.pos();
                 count ++;
@@ -146,13 +238,27 @@ struct Boid {
         }
         if (count > 0){
             sum /= count;
-            sum = sum + velocity * 10; //position + velocity = temporary target to look at
+            sum = sum + velocity * 10; 
+            //position + velocity = temporary target to look at
         } 
-        Vec3f src = Vec3f(pose.quat().toVectorZ()).normalize();
-        Vec3f dst = Vec3f(sum - pose.pos()).normalize();
-        Quatd rot = Quatd::getRotationTo(src,dst);
-        pose.quat() =  rot * pose.quat();
-        //pose.faceToward(velocity.cross(pose.pos()),1);
+
+        //once find target nearby, arrive and follow it
+        if (min > 0 && min < target_senseRadius){
+            Vec3f ar(arrive(tg.targets[min_id].position));
+            ar *= 1.0;
+            applyForce(ar);
+            Vec3f src = Vec3f(pose.quat().toVectorZ()).normalize();
+            Vec3f dst = Vec3f(tg.targets[min_id].position - pose.pos()).normalize();
+            Quatd rot = Quatd::getRotationTo(src,dst);
+            pose.quat() =  rot * pose.quat();
+            
+        } else {
+            //if no target nearby, look at where clan is moving to
+            Vec3f src = Vec3f(pose.quat().toVectorZ()).normalize();
+            Vec3f dst = Vec3f(sum - pose.pos()).normalize();
+            Quatd rot = Quatd::getRotationTo(src,dst);
+            pose.quat() =  rot * pose.quat();
+        }
     }
 
     void applyForce(Vec3f force){
@@ -161,7 +267,6 @@ struct Boid {
       if (acceleration.mag() > maxAcceleration){
         acceleration.normalize(maxAcceleration);
       }
-     
     }
 
     Vec3f separate(vector<Boid> c){
@@ -195,7 +300,7 @@ struct Boid {
         for (Boid other : c) {
             Vec3f difference = pose.pos() - other.pose.pos();
             double d = difference.mag();
-            if ((d > 0) && (d < senseRadius)){
+            if ((d > 0) && (d < neighbour_senseRadius)){
                 sum += other.velocity;
                 count ++;
             }
@@ -219,7 +324,7 @@ struct Boid {
         for (Boid other : c){
             Vec3f difference = pose.pos() - other.pose.pos();
             double d = difference.mag();
-            if ((d > 0) && (d < senseRadius)){
+            if ((d > 0) && (d < neighbour_senseRadius)){
                 sum += other.pose.pos();
                 count ++;
             }
@@ -236,27 +341,39 @@ struct Boid {
         Vec3f sep(separate(c));
         Vec3f ali(alignment(c));
         Vec3f coh(cohesion(c));
-        sep *= 1.5;
+        Vec3f bd(border_detect());
+        sep *= 2.0;
         ali *= 1.0;
         coh *= 1.0;
+        bd *= 1.5;
         applyForce(sep);
         applyForce(ali);
         applyForce(coh);
+        applyForce(bd);
     }
 
+    //some sort of gravitational force from the center, also initial force
     void seekCenter(){
-        Vec3f target(0,0,0);
-        Vec3f sk(seek(target));
-        sk *= 1.0;
-        applyForce(sk);
+        Vec3f center(0,0,0);
+        Vec3f sk(seek(center));
+            sk *= 0.3;
+            applyForce(sk);
     }
 
-    // void border_detect(){
-    //     Vec3f distance = pose.pos();
-    //     if (distance.x > borderRadius) {
-    //         Vec3f desire; 
-    //     }
-    // }
+    Vec3f border_detect(){
+        Vec3f origin(0,0,0);
+        Vec3f distance = pose.pos() - origin;
+        if (distance.mag() > boundary_radius) {
+            Vec3f desired = origin - pose.pos();
+            Vec3f steer = desired - velocity;
+            if (steer.mag() > maxforce) {
+                steer.normalize(maxforce);
+            }
+            return steer;
+        } else {
+            return Vec3f(0,0,0);
+        }
+    }
 
     void draw(Graphics& g) {
         g.pushMatrix();
@@ -276,6 +393,7 @@ struct Clan {
 
     Clan(){
     }
+
     Boid operator [](const int index) const {
         return boids[index];
     }
@@ -285,28 +403,21 @@ struct Clan {
 
     void grow(){
         Boid b;
-        //b.position = origin; //all start from one origin
         boids.push_back(b);
     }
     void draw(Graphics& g){
         for (int i = boids.size() - 1; i >= 0; i--){
             Boid& b = boids[i];
             b.draw(g);
-            // if (b.isDead()){
-            //     b.erase(boids.begin() + i);
-            // }
         }
     }
-    void run(){
+    void run(Target_group tg){
          for (Boid& b : boids) {
             b.update();
             b.flock(boids);
             b.seekCenter();
-            b.follow_group_direction(boids);
+            b.find_direction(boids, tg);
         }
-        //  if (boids.size() > 0){
-        //      cout << boids[0].pose.quat().x << " x " << boids[0].pose.quat().y << " y " << boids[0].pose.quat().z << " z " << boids[0].pose.quat().w<< endl; 
-        //  }
     }
 };
 
@@ -343,22 +454,24 @@ struct Clan {
 //     }
 // };
 
-
-
 struct MyApp : App {
   Material material;
   Light light;
   bool simulate = true;
   bool grow = true;
+  bool target_run = true;
   float t = 0;
   float timer_speed = 1.5;
 
   Clan c;
+  Target_group tg;
 
   MyApp() {
     //basic settings
     addCone(cone, coneRadius, Vec3f(0,0,coneHeight));
     cone.generateNormals();
+    addSphere(sphere, sphereRadius);
+    sphere.generateNormals();
     light.pos(0, 0, 0);              // place the light
     nav().pos(0, 0, 30);             // place the viewer
     lens().far(400);                 // set the far clipping plane
@@ -386,14 +499,22 @@ struct MyApp : App {
         t = 0.0;
     }
 
-     if (grow) {
+    if (grow) {
         if (t == 0.0){
             if (c().size() < clan_population_max){
                 c.grow();
             }
         }
     }
-    c.run();
+
+
+    c.run(tg);
+    if (target_run){
+        if (tg.targets.size() < 3){
+            tg.add_target();
+        }
+        tg.applyBehaviors();
+    }
 
   }
 
@@ -402,6 +523,7 @@ struct MyApp : App {
     light();
     g.scale(scaleFactor);
     c.draw(g);
+    tg.draw(g);
   }
 
   void onSound(AudioIO& io) {
@@ -422,7 +544,7 @@ struct MyApp : App {
         simulate = !simulate;
         break;
       case '3':
-        
+        target_run = !target_run;
         break;
       case '4':
        
